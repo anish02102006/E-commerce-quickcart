a051d8bab6f3126fb6e91a3143c5138a65556404
Srajal puri <sra@student.iul.ac.in>
2025-04-06 10:40:56 +0530

    Update

--- config/inngest.js
+++ config/inngest.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
-import { connect } from "mongoose";
+import connectDB from "./db";
+import {User} from "@/models/user"
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });

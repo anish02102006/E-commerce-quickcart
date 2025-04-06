42e9520ab41465b757f74f5469a287dfeeb02171
Srajal puri <sra@student.iul.ac.in>
2025-04-06 10:10:31 +0530

    Update

--- config/inngest.js
+++ config/inngest.js
@@ -9,7 +9,7 @@ export const syncUserCreation = inngest.createFunction (
         id:'sync-user-from-clerk'
     },
     {event: 'clerk/user.created'},
-    async (event) => {
+    async ({event}) => {
 
         const { id,first_name, last_name,email_addresses, image_url } = event.data
 

e1ab9133e0d38345f316342f968abc89430b2ca7
sanjay <sanjaygolappanvar1@gmail.com>
2025-05-02 21:01:53 +0530

    fifth commit

--- config/inngest.js
+++ config/inngest.js
@@ -1,4 +1,4 @@
-import { clerkClient } from "@clerk/nextjs/dist/types/server";
+import { clerkClient } from "@clerk/nextjs/server";
 import { Inngest } from "inngest";
 import connectDB from "./db";
 import User from "@/models/user";

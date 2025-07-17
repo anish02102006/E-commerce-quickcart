4459bc24b33655b185890f77d7311bacbc14a58c
gigitiminkova <missaouihadjer29@gmail.com>
2025-07-17 11:26:08 +0100

    modified:   .env modified:   components/Navbar.jsx modified:   context/AppContext.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,16 @@
 "use client"
 import React from "react";
-import { assets} from "@/assets/assets";
+import { assets } from "@/assets/assets";
 import Link from "next/link"
 import { useAppContext } from "@/context/AppContext";
+import { useClerk, UserButton } from "@clerk/nextjs"; // ← Import manquant
 import Image from "next/image";
 
 const Navbar = () => {
 
-  const { isSeller, router } = useAppContext();
-  const {openSignIn} = useClerk()
+  const { isSeller, router, user } = useAppContext();
+  const { openSignIn } = useClerk(); // ← Utilisation correcte du hook
+
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
       <Image
@@ -37,10 +39,16 @@ const Navbar = () => {
 
       <ul className="hidden md:flex items-center gap-4 ">
         <Image className="w-4 h-4" src={assets.search_icon} alt="search icon" />
-        <button onClick={openSignIn} className="flex items-center gap-2 hover:text-gray-900 transition">
-          <Image src={assets.user_icon} alt="user icon" />
-          Account
-        </button>
+        {
+          user
+            ? <>
+            <UserButton/>
+            </>
+            : <button onClick={openSignIn} className="flex items-center gap-2 hover:text-gray-900 transition">
+              <Image src={assets.user_icon} alt="user icon" />
+              Account
+            </button>
+        }
       </ul>
 
       <div className="flex items-center md:hidden gap-3">

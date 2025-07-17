9f1191ce85e301bf8acc14eecd3b641a794f7976
Minenhle Cyril Cele <minenhlecyrilcele2@gmail.com>
2025-07-17 08:50:22 +0200

    First Commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -4,7 +4,7 @@ import {
   syncUserCreation,
   syncUserDeletion,
   syncUserUpdation,
-} from "../../../config/inngest";
+} from "@/components/config/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
